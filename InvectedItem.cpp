31a26031d3772bd10282daacf9f943cc06f614c0
Yoshi31 <62884580+Yoshi31@users.noreply.github.com>
2020-05-21 13:07:29 +0300

    Add files via upload

---
+++ InvectedItem.cpp
@@ -0,0 +1,1 @@
+#include "InvectedItem.h"
---
+++ Virus.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+class Virus
+{
+private:
+	string nameVirus;//Название вируса
+	double startInvectionRate;//Стартовая скорость заражения
+	double mortality;//Смертность
+	int periodOfIllnes;//период болезни
+	int zeroDay;//первый день болезни
+	bool vaccine;//наличие вакцины
+public:
+	//прописать геттеры, сеттеры, конструктор
+};
+
