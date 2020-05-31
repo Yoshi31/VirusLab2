f90465c26818393ea55a09de8008bdba55a3bc0d
Yoshi31 <62884580+Yoshi31@users.noreply.github.com>
2020-05-31 17:33:08 +0300

    Update VirusLabNumber2.cpp

--- VirusLabNumber2.cpp
+++ VirusLabNumber2.cpp
@@ -10,6 +10,11 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     setlocale(LC_CTYPE, "Russian");
+       if(argc!=3)
+    {
+        cout<<"Неверное колличество параметров!"<<endl;
+        return 1;
+    }
     cout << "Добро пожаловать в программу, которая поможет вам сымитировать модель вируса на конкретном городе!" << endl;
     menu(argv[1],argv[2]);
     return 0;
