208450870bdb7da4a0f9fe5f0e8dec1d0c6fa107
Yoshi31 <62884580+Yoshi31@users.noreply.github.com>
2020-06-05 20:33:47 +0300

    Update InvectedItem.h

--- InvectedItem.h
+++ InvectedItem.h
@@ -2,33 +2,32 @@
 #include <string>
 #include <iostream>
 using namespace std;
-//класс отвечающий за зараженный город
+//РєР»Р°СЃСЃ РѕС‚РІРµС‡Р°СЋС‰РёР№ Р·Р° Р·Р°СЂР°Р¶РµРЅРЅС‹Р№ РіРѕСЂРѕРґ
 class InfectedItem
 {
 private:
-	string cityName;//название города
-	int populationDensity;//колличество населения
-	double secutiryLevel;//уровень безопасности города(мытье рук, санитария и т.д.)
-	int levelOfMedicine;//уровень медицины(влияет на смертность и на выздоравлиемость)
+	string cityName;//РЅР°Р·РІР°РЅРёРµ РіРѕСЂРѕРґР°
+	int populationDensity;//РєРѕР»Р»РёС‡РµСЃС‚РІРѕ РЅР°СЃРµР»РµРЅРёСЏ
+	double secutiryLevel;//СѓСЂРѕРІРµРЅСЊ Р±РµР·РѕРїР°СЃРЅРѕСЃС‚Рё РіРѕСЂРѕРґР°(РјС‹С‚СЊРµ СЂСѓРє, СЃР°РЅРёС‚Р°СЂРёСЏ Рё С‚.Рґ.)
+	int levelOfMedicine;//СѓСЂРѕРІРµРЅСЊ РјРµРґРёС†РёРЅС‹(РІР»РёСЏРµС‚ РЅР° СЃРјРµСЂС‚РЅРѕСЃС‚СЊ Рё РЅР° РІС‹Р·РґРѕСЂР°РІР»РёРµРјРѕСЃС‚СЊ)
 public:
-	//конструктор
+	//РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ
 	InfectedItem();
-	//сеттеры
+	//СЃРµС‚С‚РµСЂС‹
 	void setCityName(string name);
 	void setPopulationDensity(int population);
 	void setSecurityLevel(double percentSecurity);
 	void setLevelOfMedicine(int medicine);
-	//геттеры
+	//РіРµС‚С‚РµСЂС‹
 	string getCityName();
 	int getPopulationDensity();
 	double getSecurityLevel();
 	int getLevelOfMedicine();
-	//заполнения класса зараженного города пользователем
+	//Р·Р°РїРѕР»РЅРµРЅРёСЏ РєР»Р°СЃСЃР° Р·Р°СЂР°Р¶РµРЅРЅРѕРіРѕ РіРѕСЂРѕРґР° РїРѕР»СЊР·РѕРІР°С‚РµР»РµРј
 	void introduceCity();
-	//выводит данные класса в консоль
+	//РІС‹РІРѕРґРёС‚ РґР°РЅРЅС‹Рµ РєР»Р°СЃСЃР° РІ РєРѕРЅСЃРѕР»СЊ
 	void printCity();
 	//
 	friend ostream& operator << (ostream& os, InfectedItem& city);
 	friend istream& operator >> (istream& is, InfectedItem& city);
 };
-
